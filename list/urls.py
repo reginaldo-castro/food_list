e3f339d3ff81bac99b7c45eb197772ec48eda207
reginaldo-castro <reginaldo.rc.castro@gmail.com>
2021-12-08 23:00:29 -0400

    criando as urls de category

--- list/urls.py
+++ list/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('category', views.category, name="category"),
+    path('category/new', views.category_new, name="category_new"),
+    path('category/edit/<int:id>', views.category_edit, name='category_edit'),
+    path('category/delete/<int:id>', views.category_delete, name="category_delete"),
+    path('category/<int:id>', views.category_detail, name="category_detail"),
 ]
