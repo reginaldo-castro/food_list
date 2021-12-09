8adb2ca6dcbd4ae3c40892dacf1d23ed649b9a3e
reginaldo-castro <reginaldo.rc.castro@gmail.com>
2021-12-08 23:00:03 -0400

    criando a view de category

--- list/views.py
+++ list/views.py
@@ -1,7 +1,44 @@
-from django.shortcuts import render
-from .models import Category
+from django.shortcuts import get_object_or_404, redirect, render
 
-def index(request):
-    category = Category.objects.order_by('-descricao')
-    context = {'categoy': category}
+from list.form import CategoryForm
+from .models import Category
+from django.views.generic import ListView, DetailView
+
+def category(request):
+    category = Category.objects.all()
+    context = {'category': category}
     return render(request, 'category/index.html', context)
+
+def category_detail(request, id):
+    category = Category.objects.get(id=id)
+    return render(request, 'category/detail.html', {'category': category})
+
+def category_new(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('category')
+    form = CategoryForm()
+
+    return render(request, 'category/create.html', {'form': form})
+
+def category_edit(request, id):
+    category = get_object_or_404(Category, id=id)
+    form = CategoryForm(request.POST or None, instance=category)
+    if form.is_valid():
+        form.save()
+        return redirect('category')
+    return render(request, 'category/edit.html', {'form': form})
+
+def category_delete(request, id):
+    category = get_object_or_404(Category, id=id)
+    if request.method == 'POST':
+        category.delete()
+        return redirect('category')
+    return render(request, 'category/confirm_delete.html', {'category': category})
+
+
+#senha de admin
+#reginaldo
+#dev01
