238fbcf78bf715b833130e0cb1f6277a6286cf72
reginaldo-castro <reginaldo.rc.castro@gmail.com>
2022-04-06 20:47:46 -0400

    ajuste de pasta do projeto, arquivos movidos

--- list/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . models import Category
-# Register your models here.
-
-admin.site.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('category')
